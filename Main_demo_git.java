bf98d740a4110c4adf0f5b4522ea21338c777720
st10440636 <162787141+st10440636@users.noreply.github.com>
2024-03-20 11:04:02 +0200

    Add files via upload

---
+++ Main_demo_git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main_demo_git;
+
+/**
+ *
+ * @author sammy_50quonu
+ */
+public class Main_demo_git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
